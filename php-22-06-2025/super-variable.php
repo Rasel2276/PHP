e85d1bde2a094f8afd0077a08c35ed5168c91c75
Rasel2276 <mdraselhossain2276@gmail.com>
2025-06-22 13:32:46 +0600

    up

---
+++ php-22-06-2025/super-variable.php
@@ -0,0 +1,19 @@
+
+<!-- <?php
+// $_POST দিয়ে ফর্ম ডেটা রিসিভ
+// $name = $_POST['username'];
+// $email = $_POST['email'];
+
+// echo "Name: $name <br>";
+// echo "Email: $email";
+// ?>
+<form action="handle.php" method="post">
+  Name: <input type="text" name="username"><br>
+  Email: <input type="email" name="email"><br>
+  <input type="submit" value="Submit">
+</form> -->
+
+<!-- handle.php -->
+<?php
+echo "hello";
+?>
